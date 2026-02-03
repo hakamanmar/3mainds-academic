3a608e7af6646bb2cfafce1c1c23c53836615f4a
hakamanmar <hakambmn@gmail.com>
2026-02-03 19:02:57 +0300

    Delete academic-platform directory

--- academic-platform/fix_admin.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('academic.db')
-conn.execute("UPDATE users SET role = 'admin' WHERE email = 'admin@uni.edu'")
-conn.commit()
-conn.close()
-print("Success: admin@uni.edu is now an admin.")
